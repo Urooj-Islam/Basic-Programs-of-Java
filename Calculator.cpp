7fc49c6f1b2f2854e04a3ad1cb6c531d4606e050
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2023-01-08 13:39:17 +0500

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	char op;
+	cout<<"Enter two numbers: ";
+	cin>>a>>b;
+	cout<<"Enter any operator from (+, -, *, / ) ";
+	cin>>op;
+	switch(op)
+	{
+		case '+':
+			cout<<a + b;    break;
+		case '-':
+			cout<<a - b;    break;
+		case '*':
+			cout<<a * b;    break;
+		case '/':
+			cout<<a / b;    break;
+	}
+	
+	return 0;
+}
